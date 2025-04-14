9d31e16ff581a888cde412da09aa3c9bc46afa11
K. Papadopoulos <kostashope@hotmail.com>
2025-04-14 21:12:01 +0300

    feat: jest tests added

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const request = require("supertest");
 
 const authService = require('../services/auth.service');
+const userService = require('../services/user.services');
 
 const app = require('../app');
 // require('dotenv').config();
@@ -41,4 +42,32 @@ describe("Requests for /api/users", () => {
     expect(res.body.status).toBeTruthy();
     expect(res.body.data.length).toBeGreaterThan(0);
   }, 10000);
+})
+
+describe("Requests for /api/users/:username", () => {
+  let token;
+
+  beforeAll(() => {
+    user = {
+      username: "admin",
+      email: "admin@aueb.gr",
+      roles: ["EDITOR", "READER", "ADMIN"]
+    };
+    token = authService.generateAccessToken(user);
+  });
+
+  test("Get returns specific user", async () => {
+
+    const result = await userService.findLastInsertedUser();
+    console.log('RESULt>>', result)
+
+    const res = await request(app)
+      .get('/api/users/'+result.username)
+      .set("Authorization", `Bearer ${token}`);
+
+    expect(res.statusCode).toBe(200);
+    expect(res.body.status).toBeTruthy;
+    expect(res.body.data.username).toBe(result.username);
+    expect(res.body.data.email).toBe(result.email);
+  })
 })
